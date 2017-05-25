31fe11900be7e0adbee919153e11c6ab269babc1
Raul Ovalle <raulovallet@gmail.com>
2017-05-25 16:29:32 -0400

    add in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -8,8 +8,21 @@
     'website': '',
     'license': '',
     'depends': ['account'],
-    'data': ['data/account.tax.template.xml',
-             'fiscal_position_template.xml'],
+    'data': [
+        # Basic accounting data
+        'data/l10n_do_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account_chart_template_data.xml',
+        'data/account_account_tag_data.xml',
+        'data/account.tax.template.xml',
+        # Country States
+        'data/l10n_do_state_data.xml',
+        # Adds fiscal position
+        'data/fiscal_position_template.xml',
+        # configuration wizard, views, reports...
+        'data/account_chart_template_data.yml',
+    ],
+
     'installable': True,
     'auto_install': False,
 }
